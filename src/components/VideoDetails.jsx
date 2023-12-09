34e601088bf4ef9c02708c162342dddbf743837d
Ashish Vishwakarma <ashishvishwakarma7519@gmail.com>
2023-12-09 22:07:51 +0530

    Worked on video page

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineLike, AiFillDislike, AiOutlineDislike } from "react-icons/ai";
 import { abbreviateNumber } from "js-abbreviation-number";
 
 import { fetchDataFromApi } from "../utils/api";
@@ -42,23 +42,58 @@ const VideoDetails = () => {
   return (
     <div className="flex justify-center flex-row h-[calc(100%-56px) bg-black">
       <div className="w-full max-w-[1280px] flex flex-col lg:flex-row">
-
-        <div className="flex flex-col lg:w-[calc(100%-350px] xl:w-[calc(100%-400px] px-4 py-3 lg:py-6 overflow-y-auto">
+        <div className="flex flex-col px-4 py-3 lg:py-6 overflow-y-auto">
           <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
-            <ReactPlayer 
+            <ReactPlayer
               url={`https://www.youtube.com/watch?v=${id}`}
               controls
               width="100%"
               height="100%"
-              style={{backgroundColor: '#000000'}}
+              style={{ backgroundColor: "#000000" }}
             />
           </div>
 
-          <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
-            {video?.title}
+          <div className="text-white">
+            <div className="font-bold text-sm md:text-xl mt-4 line-clamp-2">
+              {video?.title}
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <img
+                  src={video?.author?.avatar[0]?.url}
+                  alt={video?.author?.title}
+                  className="rounded-full h-[45px] w-[45px]"
+                />
+                <div className="ml-2 text-gray-200">
+                  <div className="flex items-center">
+                    <div className="font-bold mr-2">{video?.author?.title}</div>
+                    <div>
+                      {video?.author?.badges[0]?.text === "Verified" && (
+                        <BsFillCheckCircleFill />
+                      )}
+                    </div>
+                  </div>
+                  <div>{video?.author?.stats?.subscribersText}</div>
+                </div>
+              </div>
+              <div className="flex gap-3 bg-slate-800 py-2 px-3 rounded-full">
+                <button className="flex items-center gap-2 pr-3 border-r border-gray-600">
+                  <AiOutlineLike className="h-[22px] w-[22px]" />
+                  <span>{abbreviateNumber(video?.stats?.likes)}</span>
+                </button>
+                <button>
+                  <AiOutlineDislike className="h-[22px] w-[22px]" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="text-white bg-slate-900">
+            <div className="font-semibold">
+              {`${abbreviateNumber(video?.stats?.views)} Views`}
+            </div>
           </div>
         </div>
-
       </div>
     </div>
   );
--- src/shared/videoLength.jsx
+++ src/shared/videoLength.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
 
 const VideoLength = ({ time }) => {
-  const videoLengthInSeconds = moment()
-    ?.startOf("day")
-    ?.seconds(time)
-    ?.format("H:mm:ss");
+
+  const videoLengthInSeconds = moment().startOf("day").seconds(time).format("H:mm:ss");
 
   return (
-    <span className="absolute bottom-2 right-2 bg-black py-1 px-2">
+    <div className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
       {videoLengthInSeconds}
-    </span>
-  );
-};
+    </div>
+  )
+}
 
-export default VideoLength;
+export default VideoLength
